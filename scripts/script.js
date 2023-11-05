5f8f70b889eda516ad8fae556daf82192fdd9fad
mati101811 <82444114+mati101811@users.noreply.github.com>
2023-11-05 21:23:02 +0100

    All html is ready. Need to join database.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,22 @@
 var now;
 var firstDayOfWeek;
+var currentWeek = 0;
+
+var selectedDay = -1;
+var selectedHour = -1;
+var clickedDiv = null;
+
+var wizyty = [
+    { data: "2023-11-10", godzina: 10 },
+    { data: "2023-11-8", godzina: 15 },
+    { data: "2023-11-15", godzina: 14 },
+    { data: "2023-1-12", godzina: 10 },
+    { data: "2023-11-30", godzina: 14 },
+    { data: "2023-12-1", godzina: 10 },
+    { data: "2023-11-20", godzina: 16 }
+];
+
+aktualne_wizyty = [];
 
 function updateCurrentWeek(now) {
     const dateElement = document.getElementById('date');
@@ -22,24 +39,88 @@ function updateCurrentWeek(now) {
     document.getElementById('thursday-date').textContent = addDays(firstDayOfWeek, 3).toLocaleDateString('pl-PL', options);
     document.getElementById('friday-date').textContent = addDays(firstDayOfWeek, 4).toLocaleDateString('pl-PL', options);
 
-    openModal(1, 8)
+    var firstDay = new Date(firstDayOfWeek).setHours(0);
+    var lastDay = new Date(lastDayOfWeek).setHours(0);
+
+    clearCalendar();
+
+    wizyty.forEach(wizyta => {
+        if (new Date(wizyta.data).setHours(wizyta.godzina) >= firstDay && new Date(wizyta.data).setHours(wizyta.godzina) <= lastDay) {
+            var selector = '.hour[hour="' + wizyta.godzina + '"][day="' + (new Date(wizyta.data).getDay() - 1) + '"]';
+            aktualne_wizyty.push(selector);
+        }
+    });
+
+    setCalendar();
 }
+
+
+function clearCalendar() {
+    if (aktualne_wizyty != null) {
+        aktualne_wizyty.forEach(selector => {
+            var divWithAttributes = document.querySelector(selector);
+            divWithAttributes.style.backgroundColor = "#fff";
+            divWithAttributes.textContent = "Wolne";
+        });
+    }
+    aktualne_wizyty = [];
+}
+
+
+function setCalendar() {
+    if (aktualne_wizyty != null) {
+        aktualne_wizyty.forEach(selector => {
+            var divWithAttributes = document.querySelector(selector);
+            divWithAttributes.style.backgroundColor = "#b52121";
+            divWithAttributes.textContent = "Zajęte";
+        });
+    }
+}
+
 
 window.onload = getCurrentDate;
 
 function getCurrentDate() {
+    currentWeek = 0;
     now = new Date();
-    updateCurrentWeek(now)
+    updateCurrentWeek(now);
+
+    document.getElementById("left").style.backgroundColor = "#3e688a";
+    document.getElementById("left").style.borderColor = "#3e688a";
+
+    document.getElementById("right").style.backgroundColor = "#2196f3";
+    document.getElementById("right").style.borderColor = "#2196f3";
 }
 
 function weekBack() {
-    now.setDate(now.getDate() - 7);
-    updateCurrentWeek(now);
+    if (currentWeek > 0) {
+        currentWeek -= 1;
+        now.setDate(now.getDate() - 7);
+        updateCurrentWeek(now);
+        if (currentWeek == 0) {
+            document.getElementById("left").style.backgroundColor = "#3e688a";
+            document.getElementById("left").style.borderColor = "#3e688a";
+        }
+    }
+
+    document.getElementById("right").style.backgroundColor = "#2196f3";
+    document.getElementById("right").style.borderColor = "#2196f3";
 }
 
 function weekForward() {
-    now.setDate(now.getDate() + 7);
-    updateCurrentWeek(now);
+    if (currentWeek < 4) {
+        currentWeek += 1;
+        now.setDate(now.getDate() + 7);
+        updateCurrentWeek(now);
+        if (currentWeek == 4) {
+
+            document.getElementById("right").style.backgroundColor = "#3e688a";
+            document.getElementById("right").style.borderColor = "#3e688a";
+        }
+    }
+
+    document.getElementById("left").style.backgroundColor = "#2196f3";
+    document.getElementById("left").style.borderColor = "#2196f3";
 }
 
 function addDays(date, days) {
@@ -48,10 +129,18 @@ function addDays(date, days) {
     return result;
 }
 
-function openModal(day, hour) {
+function openModal(day, hour, div) {
+    if (div.textContent == "Zajęte") {
+        return;
+    }
+
     const modalBackground = document.getElementById('modalBackground');
     modalBackground.style.display = 'block';
     modalBackground.style.zIndex = 3;
+
+    selectedDay = day;
+    selectedHour = hour;
+    clickedDiv = div;
 
     var timeInput = document.getElementById("czas");
 
@@ -65,26 +154,39 @@ function openModal(day, hour) {
     timeInput.value = hours + ":" + minutes;
 
     var dateInput = document.getElementById("termin");
+    var date = addDays(firstDayOfWeek, day);
 
     // Ustaw wartość daty w formie tekstu (YYYY-MM-DD)
 
-    var year = firstDayOfWeek.getFullYear();
+    var year = date.getFullYear();
 
     // Dodaj zero z przodu, jeśli miesiąc lub dzień jest jednocyfrowy
-    var month = (firstDayOfWeek.getMonth() + 1).toString().padStart(2, '0');
-    var day = firstDayOfWeek.getDate().toString().padStart(2, '0');
+    var month = (date.getMonth() + 1).toString().padStart(2, '0');
+    var day = (date.getDate()).toString().padStart(2, '0');
 
     // Ustaw datę w polu input
-    dateInput.value = year + "-" + month + "-" + day;
+    selectedDay = year + "-" + month + "-" + day;
+    dateInput.value = selectedDay;
 
 }
 
 function closeModal() {
     const modalBackground = document.getElementById('modalBackground');
     modalBackground.style.display = 'none';
-    document.getElementById("dropbutton").textContent = "Wybierz"
+    document.getElementById("dropbutton").textContent = "Wybierz";
 }
 
+function saveModal() {
+    const modalBackground = document.getElementById('modalBackground');
+    modalBackground.style.display = 'none';
+    document.getElementById("dropbutton").textContent = "Wybierz";
+    clickedDiv.style.backgroundColor = "#b52121";
+    clickedDiv.textContent = "Zajęte";
+    wizyty.push({ data: selectedDay, godzina: selectedHour });
+    var selector = '.hour[hour="' + selectedHour + '"][day="' + new Date(selectedDay).getDay() + '"]';
+    aktualne_wizyty.push(selector);
+    updateCurrentWeek(firstDayOfWeek);
+}
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
